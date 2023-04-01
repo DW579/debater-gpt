e984d42c901d343b6cf2dc42dc462bb41afa2ecc
Dustin Wurtz <dustinwurtz@Dustins-MacBook-Pro.local>
2023-03-31 20:53:48 -0700

    Added react router and created component pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./components/home";
+import About from "./components/about";
+import Debate from "./components/debate";
+
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 
 function App() {
     const [data, setData] = React.useState(null);
@@ -18,7 +26,25 @@ function App() {
         <Container fluid>
             <Row>
                 <Col>
-                    <h1>Hello from React</h1>
+                    <Navbar bg="primary" variant="dark">
+                        <Navbar.Brand href="/">Debater-GPT</Navbar.Brand>
+                        <Nav className="mr-auto">
+                            <Nav.Link href="/">Home</Nav.Link>
+                            <Nav.Link href="/debate">Debate</Nav.Link>
+                            <Nav.Link href="/about">About</Nav.Link>
+                        </Nav>
+                    </Navbar>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Router>
+                        <Routes>
+                            <Route exact path="/" element={<Home />} />
+                            <Route path="/debate" element={<Debate />} />
+                            <Route path="/about" element={<About />} />
+                        </Routes>
+                    </Router>
                 </Col>
             </Row>
         </Container>
---
+++ client/src/components/debate.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Debate() {
+    return <h2>Debate</h2>;
+}
