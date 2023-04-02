bdede982e6b63744e0a7fab0faaa83dc6a720cc5
Dustin Wurtz <dustinwurtz@Dustins-MacBook-Pro.local>
2023-04-01 19:02:08 -0700

    Created opponent.js component

---
+++ client/src/components/opponent.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+// Bootstrap components
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+export default function Opponent(props) {
+    const image_style = {
+        width: "150px",
+        height: "150px",
+    };
+
+    return (
+        <Row>
+            <Col>
+                <h3>{props.name}</h3>
+                <img src={props.image} style={image_style} />
+            </Col>
+        </Row>
+    );
+}
--- client/src/pages/debate.js
+++ client/src/pages/debate.js
@@ -6,12 +6,27 @@ import Col from "react-bootstrap/Col";
 
 // Components
 import Headline from "../components/headline";
+import Opponent from "../components/opponent";
 
 export default function Debate() {
     return (
         <Row className="justify-content-center">
             <Col xs={12} md={6} className="text-center">
                 <Headline name="Debate!" />
+                <Row>
+                    <Col>
+                        <Opponent
+                            name="Opponent 1"
+                            image="https://via.placeholder.com/150"
+                        />
+                    </Col>
+                    <Col>
+                        <Opponent
+                            name="Opponent 2"
+                            image="https://via.placeholder.com/150"
+                        />
+                    </Col>
+                </Row>
             </Col>
         </Row>
     );
