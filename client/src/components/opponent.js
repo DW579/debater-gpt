bc12eb20f7e48d4b9573f86b3d81738c1223be02
Dustin Wurtz <dustinwurtz@Dustins-MacBook-Pro.local>
2023-04-03 18:16:27 -0700

    Implemented opponent and topic selections into modals

--- client/src/components/opponent.js
+++ client/src/components/opponent.js
@@ -3,63 +3,18 @@ import React from "react";
 // Bootstrap components
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Dropdown from "react-bootstrap/Dropdown";
-
-// Images
-import QuestionMark from "../images/question_mark.png";
-import OpenaiLogo from "../images/openai_logo.png";
-import UserImage from "../images/user.jpg";
 
 export default function Opponent(props) {
-    // State variables for opponent and opponent image
-    const [opponent, setOpponent] = React.useState("");
-    const [opponentImage, setOpponentImage] = React.useState(QuestionMark);
-
     const image_style = {
         width: "150px",
         height: "150px",
-    };
-
-    // Function to handle opponent selection from dropdown menu
-    const handleSelect = (eventKey) => {
-        let opponent = "";
-
-        // Set opponent based on eventKey value (1 or 2) and update state with setOpponent function
-        if (eventKey === "1") {
-            setOpponent("Chat-GPT 3");
-            setOpponentImage(OpenaiLogo);
-
-            opponent = "Chat-GPT 3";
-        } else if (eventKey === "2") {
-            setOpponent("Human");
-            setOpponentImage(UserImage);
-
-            opponent = "Human";
-        }
-
-        // Call the onData function passed in as a prop from Debate.js
-        props.onData(opponent);
     };
 
     return (
         <Row>
             <Col>
                 <h3>{props.name}</h3>
-                <img src={opponentImage} style={image_style} alt="Opponent" />
-                <Dropdown onSelect={handleSelect}>
-                    <Dropdown.Toggle
-                        variant="success"
-                        id="dropdown-basic"
-                        disabled={props.dropdown}
-                    >
-                        {opponent ? opponent : "Select Opponent"}
-                    </Dropdown.Toggle>
-
-                    <Dropdown.Menu>
-                        <Dropdown.Item eventKey="1">Chat-GPT 3</Dropdown.Item>
-                        <Dropdown.Item eventKey="2">Human</Dropdown.Item>
-                    </Dropdown.Menu>
-                </Dropdown>
+                <img src={props.image} style={image_style} alt="Opponent" />
             </Col>
         </Row>
     );
--- client/src/pages/debate.js
+++ client/src/pages/debate.js
@@ -1,84 +1,170 @@
 import React, { useEffect } from "react";
+import { FormControl } from 'react-bootstrap';
 
 // Bootstrap components
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Modal from "react-bootstrap/Modal";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Button from "react-bootstrap/esm/Button";
+
 
 // Components
 import Headline from "../components/headline";
 import Opponent from "../components/opponent";
-import Topic from "../components/topic";
-import Button from "react-bootstrap/esm/Button";
+
+// Images
+import QuestionMark from "../images/question_mark.png";
+import OpenaiLogo from "../images/openai_logo.png";
+import UserImage from "../images/user.jpg";
 
 export default function Debate() {
-    // State variables for positive and negative opponents and dropdown state variables for each opponent (true or false)
-    const [positive, setPositive] = React.useState("");
-    const [negative, setNegative] = React.useState("");
-    const [opponentDropdown, setOpponentDropdown] = React.useState(false);
+    // State variables for opponent and opponent image
+    const [positiveOpponent, setPositiveOpponent] = React.useState("Select Opponent");
+    const [negativeOpponent, setNegativeOpponent] = React.useState("Select Opponent");
+    const [positiveImage, setPositiveImage] = React.useState(QuestionMark);
+    const [negativeImage, setNegativeImage] = React.useState(QuestionMark);
 
-    // State variable for disabled button
-    const [disabled, setDisabled] = React.useState(true);
+    // State variables for topic
+    const [topic, setTopic] = React.useState("");
 
-    const [showTopic, setShowTopic] = React.useState(false);
+    // State variable for disabling opponent button
+    const [disableOpponentButton, setDisableOpponentButton] = React.useState(true);
 
-    // useEffect hook to check if both opponents have been selected and update disabled state accordingly
+    // State variables for modals
+    const [showOpponentModal, setShowOpponentModal] = React.useState(true);
+    const [showTopicModal, setShowTopicModal] = React.useState(false);
+
+    // Function to handle opponent selection from dropdown menu
     useEffect(() => {
-        if (positive && negative) {
-            setDisabled(false);
+        if (
+            positiveOpponent !== "Select Opponent" &&
+            negativeOpponent !== "Select Opponent"
+        ) {
+            setDisableOpponentButton(false);
         }
-    }, [positive, negative]);
+    }, [positiveOpponent, negativeOpponent]);
 
-    // Functions to handle positive and negative opponent selection
     const handlePositiveSelect = (data) => {
-        setPositive(data);
+        if (data === "chat-gpt") {
+            setPositiveImage(OpenaiLogo);
+            setPositiveOpponent("Chat-GPT");
+        } else if (data === "human") {
+            setPositiveImage(UserImage);
+            setPositiveOpponent("Human");
+        }
     };
 
     const handleNegativeSelect = (data) => {
-        setNegative(data);
+        if (data === "chat-gpt") {
+            setNegativeImage(OpenaiLogo);
+            setNegativeOpponent("Chat-GPT");
+        } else if (data === "human") {
+            setNegativeImage(UserImage);
+            setNegativeOpponent("Human");
+        }
     };
 
-    // Function to handle topic button click
-    const handleChooseTopicClick = () => {
-        setOpponentDropdown(true);
-        setShowTopic(true);
+    const handleOpponentNext = () => {
+        setShowOpponentModal(false);
+        setShowTopicModal(true);
+    };
+
+    const handleTopicNext = () => {
+        setShowTopicModal(false);
+    };
+
+    const handleTopicChange = (event) => {
+        setTopic(event.target.value);
     };
 
     return (
         <Row className="justify-content-center">
             <Col xs={12} md={6} className="text-center">
                 <Headline name="Debate!" />
-                <Row>
-                    <Col>
-                        <Opponent
-                            name="Positive"
-                            onData={handlePositiveSelect}
-                            dropdown={opponentDropdown}
+
+                <Modal show={showOpponentModal}>
+                    <Modal.Header>
+                        <Modal.Title>Choose your Opponenets</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Row>
+                            <Col className="text-center">
+                                <Opponent
+                                    name="Positive"
+                                    image={positiveImage}
+                                />
+                                <DropdownButton
+                                    id="dropdown-basic-button"
+                                    title={positiveOpponent}
+                                    onSelect={handlePositiveSelect}
+                                >
+                                    <Dropdown.Item eventKey="chat-gpt">
+                                        Chat-GPT
+                                    </Dropdown.Item>
+                                    <Dropdown.Item eventKey="human">
+                                        Human
+                                    </Dropdown.Item>
+                                </DropdownButton>
+                            </Col>
+                            <Col className="text-center">
+                                <Opponent
+                                    name="Negative"
+                                    image={negativeImage}
+                                />
+                                <DropdownButton
+                                    id="dropdown-basic-button"
+                                    title={negativeOpponent}
+                                    onSelect={handleNegativeSelect}
+                                >
+                                    <Dropdown.Item eventKey="chat-gpt">
+                                        Chat-GPT
+                                    </Dropdown.Item>
+                                    <Dropdown.Item eventKey="human">
+                                        Human
+                                    </Dropdown.Item>
+                                </DropdownButton>
+                            </Col>
+                        </Row>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button
+                            variant="primary"
+                            disabled={disableOpponentButton}
+                            onClick={handleOpponentNext}
+                        >
+                            Next
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+                <Modal show={showTopicModal}>
+                    <Modal.Header>
+                        <Modal.Title>Choose your Topic</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <FormControl
+                            type="text"
+                            placeholder="Enter Topic"
+                            onChange={handleTopicChange}
                         />
-                    </Col>
-                    <Col>
-                        <Opponent
-                            name="Negative"
-                            onData={handleNegativeSelect}
-                            dropdown={opponentDropdown}
-                        />
-                    </Col>
-                </Row>
-                {showTopic ? (
-                    <Topic />
-                ) : (
-                    <Row>
-                        <Col>
-                            <Button
-                                variant="success"
-                                size="lg"
-                                disabled={disabled}
-                                onClick={handleChooseTopicClick}
-                            >
-                                Choose Topic
-                            </Button>
-                        </Col>
-                    </Row>
-                )}
+                        <h6>Here are a few examples to copy and paste:</h6>
+                        <ul>
+                            <li>
+                                Should Seattle, WA make public transportation
+                                free?
+                            </li>
+                            <li>
+                                Should the US have a national healthcare system?
+                            </li>
+                        </ul>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="primary" onClick={handleTopicNext}>
+                            Begin Debate
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
             </Col>
         </Row>
     );
