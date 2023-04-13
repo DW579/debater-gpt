d12c82c69665893de3befb821817badfb829b661
Dustin Wurtz <dustinwurtz@Dustins-MacBook-Pro.local>
2023-04-12 18:15:20 -0700

    Deploy app to Heroku

--- server/index.js
+++ server/index.js
@@ -16,6 +16,9 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+
 app.post("/argument-gpt", (req, res) => {
     const prompt = req.body.prompt;
     const opponent = req.body.opponent;
@@ -134,4 +137,9 @@ app.post("/argument-user", (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
+});
+
+// All other GET requests not handled before will return our React app
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
